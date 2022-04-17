1c87bd568fcd173798aaf335fdc0eeffc625262a
Noah <hi@noahflk.com>
2022-04-17 15:12:15 +0200

    add social OG image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -11,6 +11,18 @@ const Head: React.FC<Props> = ({ title }) => (
       {process.env.NEXT_PUBLIC_APP_NAME}
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content={process.env.NEXT_PUBLIC_APP_NAME} />
+    <meta property="og:description" content="Keep track of your journeys with public transport in and around Switzerland." />
+    <meta property="og:site_name" content={process.env.NEXT_PUBLIC_APP_NAME} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@noahflk" />
+    <meta property="og:url" content={process.env.NEXT_PUBLIC_APP_URL} />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content={`${process.env.NEXT_PUBLIC_APP_URL}/images/social.jpg`} />
+    <meta property="og:image:alt" content={`${process.env.NEXT_PUBLIC_APP_NAME} social image`} />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:locale" content="en_US" />
   </NextHead>
 );
 
