bcd8642180eb2c3bbfc1d03e37de27e230c1d7c6
Noah <hi@noahflk.com>
2022-04-17 14:45:21 +0200

    Add empty state for recent journeys view

--- src/components/dashboard/RecentJourneys.tsx
+++ src/components/dashboard/RecentJourneys.tsx
@@ -1,5 +1,8 @@
+import { DocumentSearchIcon } from '@heroicons/react/outline';
+
 import useRecentJourneys from '@/hooks/useRecentJourneys';
 import Journey from '@/components/dashboard/Journey';
+import Link from '@/components/Link';
 
 const RecentJourneys: React.FC = () => {
   const { data: journeys } = useRecentJourneys();
@@ -9,11 +12,19 @@ const RecentJourneys: React.FC = () => {
   return (
     <div className="col-span-1 p-4 bg-white rounded-lg shadow">
       <h3 className="text-xl font-medium text-gray-900">Recent trips</h3>
-      <ul>
-        {journeys.map((journey) => (
-          <Journey key={journey.id} journey={journey} />
-        ))}
-      </ul>
+      {journeys.length > 0 ? (
+        <ul>
+          {journeys.map((journey) => (
+            <Journey key={journey.id} journey={journey} />
+          ))}
+        </ul>
+      ) : (
+        <div className="flex flex-col items-center justify-center h-full pb-8 space-y-2">
+          <DocumentSearchIcon className="w-20 " />
+          <p className="text-lg font-medium">No journeys yet!</p>
+          <Link href="/add">Add new journey</Link>
+        </div>
+      )}
     </div>
   );
 };
