102d342acac7cc5fecc127cab707858914dc86ae
Noah <hi@noahflk.com>
2022-04-23 17:44:18 +0200

    see all journeys tweak

--- src/components/dashboard/RecentJourneys.tsx
+++ src/components/dashboard/RecentJourneys.tsx
@@ -12,12 +12,16 @@ const RecentJourneys: React.FC = () => {
     <div className="col-span-1 p-4 bg-white rounded-lg shadow">
       <h3 className="text-xl font-medium text-gray-900">Recent journeys</h3>
       {journeys.length > 0 ? (
-        <ul>
-          {journeys.map((journey) => (
-            <Journey key={journey.id} journey={journey} />
-          ))}
-          <Link href="/journeys">See all journeys</Link>
-        </ul>
+        <div className="flex flex-col justify-between h-full pb-5">
+          <ul>
+            {journeys.map((journey) => (
+              <Journey key={journey.id} journey={journey} />
+            ))}
+          </ul>
+          <div>
+            <Link href="/journeys">See all journeys</Link>
+          </div>
+        </div>
       ) : (
         <EmptyJourneyNotice />
       )}
