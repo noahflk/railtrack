9c922982cd075bbb43602d36a40af39eb9e5d462
Noah <hi@noahflk.com>
2022-05-31 22:08:20 +0200

    Make loading spinner reuseable

--- src/components/auth/SignInForm.tsx
+++ src/components/auth/SignInForm.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 
 import { supabase } from '@/utils/supabaseClient';
 import Link from '@/components/Link';
+import LoadingSpinner from '@/components/LoadingSpinner';
 
 const SignInForm: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -89,18 +90,7 @@ const SignInForm: React.FC = () => {
           disabled={loading}
           className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
         >
-          {loading ? (
-            <svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          ) : (
-            'Sign In'
-          )}
+          {loading ? <LoadingSpinner /> : 'Sign In'}
         </button>
       </div>
     </form>
--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import { supabase } from '@/utils/supabaseClient';
+import LoadingSpinner from '@/components/LoadingSpinner';
 
 const SignUpForm: React.FC = () => {
   const [email, setEmail] = useState('');
@@ -83,18 +84,7 @@ const SignUpForm: React.FC = () => {
           disabled={loading}
           className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-primary hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
         >
-          {loading ? (
-            <svg className="w-5 h-5 mr-3 -ml-1 text-white animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          ) : (
-            'Sign Up'
-          )}
+          {loading ? <LoadingSpinner /> : 'Sign Up'}
         </button>
       </div>
     </form>
