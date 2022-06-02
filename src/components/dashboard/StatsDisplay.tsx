58f2c6ebec3b9bc3291b28c5766286498849efe2
Noah <hi@noahflk.com>
2022-06-02 11:27:58 +0200

    Improves loading for StatsDisplay

--- src/components/dashboard/StatsDisplay.tsx
+++ src/components/dashboard/StatsDisplay.tsx
@@ -6,24 +6,20 @@ import Stat from '@/components/dashboard/Stat';
 const StatsDisplay: React.FC = () => {
   const { data } = useStats();
 
-  if (!data) return <p>Loading...</p>;
-
-  const { distance, duration, count } = data;
-
   return (
     <ul role="list" className="grid grid-cols-1 gap-6 lg:grid-cols-3">
       <Stat title="Distance travelled" icon={<GlobeIcon className="flex-shrink-0 w-14 h-14 sm:hidden xl:inline" />}>
         <p className="mt-2 text-3xl font-medium truncate">
-          {distance} <span className="text-xl text-gray-500">km</span>
+          {data ? data.distance : '...'} <span className="text-xl text-gray-500">km</span>
         </p>
       </Stat>
       <Stat title="Time spent travelling" icon={<ClockIcon className="flex-shrink-0 w-14 h-14 sm:hidden xl:inline" />}>
         <p className="mt-2 text-3xl font-medium truncate">
-          {duration} <span className="text-xl text-gray-500">hours</span>
+          {data ? data.duration : '...'} <span className="text-xl text-gray-500">hours</span>
         </p>
       </Stat>
       <Stat title="Total trips" icon={<ChartBarIcon className="flex-shrink-0 w-14 h-14 sm:hidden xl:inline" />}>
-        <p className="mt-2 text-3xl font-medium truncate">{count}</p>
+        <p className="mt-2 text-3xl font-medium truncate">{data ? data.count : '...'}</p>
       </Stat>
     </ul>
   );
