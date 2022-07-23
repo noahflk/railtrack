230b9644b4f40a6c62b7931621047837ea0de95b
Noah <hi@noahflk.com>
2022-07-23 22:57:00 +0200

    Add redirects to next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/signin',
+        permanent: true,
+      },
+      {
+        source: '/logout',
+        destination: '/signout',
+        permanent: true,
+      },
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
