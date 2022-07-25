6c0befb74e44e4fd39c0b60aeeae99e999fe079e
Noah <hi@noahflk.com>
2022-07-25 23:24:54 +0200

    Add endpoint to get all or a limited number of connections for user

--- src/server/router/connection.ts
+++ src/server/router/connection.ts
@@ -33,71 +33,93 @@ const findConnection = async ({
   );
 };
 
-export const connectionRouter = createRouter().mutation('add', {
-  // this information is enough to precicely find the precise connection again
-  // that way we avoid passing the whole connection object from the client to the server
-  input: z.object({
-    departureStation: z.string(),
-    arrivalStation: z.string(),
-    departureTime: z.string(),
-    platform: z.string(),
-  }),
-  async resolve({ input, ctx }) {
-    const connection = await findConnection(input);
+export const connectionRouter = createRouter()
+  .mutation('add', {
+    // this information is enough to precicely find the precise connection again
+    // that way we avoid passing the whole connection object from the client to the server
+    input: z.object({
+      departureStation: z.string(),
+      arrivalStation: z.string(),
+      departureTime: z.string(),
+      platform: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const connection = await findConnection(input);
 
-    const { user } = await supabase.auth.api.getUserByCookie(ctx.req);
+      const { user } = await supabase.auth.api.getUserByCookie(ctx.req);
 
-    if (!connection) {
-      throw new TRPCError({ code: 'NOT_FOUND', message: 'Connection not found' });
-    }
+      if (!connection) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'Connection not found' });
+      }
 
-    if (!user) {
-      throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Could not find authenticated user' });
-    }
+      if (!user) {
+        throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Could not find authenticated user' });
+      }
 
-    const sections = connection.sections.filter((section) => section.journey);
+      const sections = connection.sections.filter((section) => section.journey);
 
-    // create structured data for connection to save it
-    const sectionsData = sections.map((section) => {
-      const passes = section.journey.passList.map((pass) => {
+      // create structured data for connection to save it
+      const sectionsData = sections.map((section) => {
+        const passes = section.journey.passList.map((pass) => {
+          return {
+            arrivalTime: new Date(pass.arrival),
+            departureTime: new Date(pass.departure),
+            stationName: pass.station.name,
+            stationCoordinateX: pass.station.coordinate.x,
+            stationCoordinateY: pass.station.coordinate.y,
+          };
+        });
+
         return {
-          arrivalTime: new Date(pass.arrival),
-          departureTime: new Date(pass.departure),
-          stationName: pass.station.name,
-          stationCoordinateX: pass.station.coordinate.x,
-          stationCoordinateY: pass.station.coordinate.y,
+          departureTime: new Date(section.departure.departure),
+          arrivalTime: new Date(section.arrival.arrival),
+          departureStation: section.departure.station.name,
+          departureStationCoordinateX: section.departure.station.coordinate.x,
+          departureStationCoordinateY: section.departure.station.coordinate.y,
+          arrivalStation: section.arrival.station.name,
+          arrivalStationCoordinateX: section.arrival.station.coordinate.x,
+          arrivalStationCoordinateY: section.arrival.station.coordinate.y,
+          destination: section.journey.to,
+          trainOperator: section.journey.operator,
+          trainNumber: section.journey.number,
+          trainCategory: section.journey.category,
+          passes: {
+            create: passes,
+          },
         };
       });
 
-      return {
-        departureTime: new Date(section.departure.departure),
-        arrivalTime: new Date(section.arrival.arrival),
-        departureStation: section.departure.station.name,
-        departureStationCoordinateX: section.departure.station.coordinate.x,
-        departureStationCoordinateY: section.departure.station.coordinate.y,
-        arrivalStation: section.arrival.station.name,
-        arrivalStationCoordinateX: section.arrival.station.coordinate.x,
-        arrivalStationCoordinateY: section.arrival.station.coordinate.y,
-        destination: section.journey.to,
-        trainOperator: section.journey.operator,
-        trainNumber: section.journey.number,
-        trainCategory: section.journey.category,
-        passes: {
-          create: passes,
+      await ctx.prisma.connection.create({
+        data: {
+          duration: parseDurationString(connection.duration),
+          userId: user.id,
+          sections: {
+            create: sectionsData,
+          },
         },
-      };
-    });
+      });
 
-    await ctx.prisma.connection.create({
-      data: {
-        duration: parseDurationString(connection.duration),
-        userId: user.id,
-        sections: {
-          create: sectionsData,
+      return { success: true };
+    },
+  })
+  .query('get', {
+    input: z.optional(z.number()),
+    async resolve({ input, ctx }) {
+      const { user } = await supabase.auth.api.getUserByCookie(ctx.req);
+
+      if (!user) {
+        throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Could not find authenticated user' });
+      }
+
+      const connections = await ctx.prisma.connection.findMany({
+        // this limits the number of returned connections if provided
+        // otherwise all will be returned
+        take: input,
+        where: {
+          userId: user.id,
         },
-      },
-    });
+      });
 
-    return { success: true };
-  },
-});
+      return connections;
+    },
+  });
