426f1008e15bc0f0d03d89e49f9070b6e09fb3f6
Noah <hi@noahflk.com>
2022-08-17 02:56:58 +0200

    Journey card page

--- src/components/journeys/JourneyCards.tsx
+++ src/components/journeys/JourneyCards.tsx
@@ -1,5 +1,6 @@
 import { format } from 'date-fns';
 import { ArrowNarrowRightIcon } from '@heroicons/react/outline';
+import { useTranslation } from 'next-i18next';
 
 import { TextButton } from '@/components/TextButton';
 import { InferQueryOutput } from '@/server/trpc-helper';
@@ -14,14 +15,15 @@ const JourneyCard: React.FC<CardProps> = ({ journey, handleDelete }) => {
   const departureTime = new Date(journey.departureTime);
   const arrivalTime = new Date(journey.arrivalTime);
 
+  const { t } = useTranslation('journeys');
+
   return (
     <li className="col-span-1 bg-white divide-y divide-gray-200 rounded-lg shadow">
       <div className="w-full p-6">
         <div className="grid flex-1 grid-cols-1 gap-2 mb-2 font-semibold text-gray-900 truncate xs:grid-cols-2">
           <div>
             <p>
-              {journey.departureStation} <ArrowNarrowRightIcon className="inline w-6 text-primary" />{' '}
-              {journey.arrivalStation}
+              {journey.departureStation} <ArrowNarrowRightIcon className="inline w-6 text-primary" /> {journey.arrivalStation}
             </p>
           </div>
           <div>
@@ -32,7 +34,9 @@ const JourneyCard: React.FC<CardProps> = ({ journey, handleDelete }) => {
           <p>
             {format(departureTime, 'HH:mm')} - {format(arrivalTime, 'HH:mm')}
           </p>
-          <p>Duration: {journey.duration} minutes</p>
+          <p>
+            Duration: {journey.duration} {t('minutes', { count: journey.duration })}
+          </p>
           <p>Distance: {journey.distance} km </p>
         </div>
         <div className="grid flex-1 grid-cols-1 gap-2 truncate xs:grid-cols-2">
@@ -40,7 +44,7 @@ const JourneyCard: React.FC<CardProps> = ({ journey, handleDelete }) => {
             {journey.stops} {journey.stops === 1 ? 'stop' : 'stops'}
           </p>
           <div className="justify-end xs:flex">
-            <TextButton onClick={() => handleDelete(journey.id)}>Delete</TextButton>
+            <TextButton onClick={() => handleDelete(journey.id)}>{t('delete')}</TextButton>
           </div>
         </div>
       </div>
