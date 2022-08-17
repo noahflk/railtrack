e88181b6ec9584e0b5b37eb87e7f16cde3aeddb3
Noah <hi@noahflk.com>
2022-08-18 01:58:22 +0200

    Add new locales

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,8 +1,8 @@
-import { createI18n } from 'noahflk-pub-test';
+import { createI18n } from 'next-international';
 
 import type Locale from './en';
 
-export const { useI18n, I18nProvider, getLocaleStaticProps } = createI18n<typeof Locale>({
+export const { useI18n, I18nProvider, getLocaleProps } = createI18n<typeof Locale>({
   en: () => import('./en'),
   de: () => import('./de'),
 });
