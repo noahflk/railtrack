5e7d036fbc5ba14b8b876fbd4cebc1c8d29f6407
Noah <hi@noahflk.com>
2022-09-16 22:23:11 +0200

    Remove unused code from email success page

--- src/pages/auth/success.tsx
+++ src/pages/auth/success.tsx
@@ -1,26 +1,14 @@
 import type { GetServerSideProps, NextPage } from 'next';
 
 import { AuthWrapper } from '@/components/auth/AuthWrapper';
-import { getLocaleProps } from '@/utils/locales';
-import { supabaseClient } from '@supabase/auth-helpers-nextjs';
-import { useRouter } from 'next/router';
 import { Link } from '@/components/Link';
+import { getLocaleProps } from '@/utils/locales';
 
-const Success: NextPage = () => {
-  const user = supabaseClient.auth.user();
-
-  const router = useRouter();
-
-  if (user) {
-    router.push('/dashboard?success=true');
-  }
-
-  return (
-    <AuthWrapper type="success">
-      <Link href="/dashboard">Go to your dashboard</Link>
-    </AuthWrapper>
-  );
-};
+const Success: NextPage = () => (
+  <AuthWrapper type="success">
+    <Link href="/dashboard">Go to your dashboard</Link>
+  </AuthWrapper>
+);
 
 export const getServerSideProps: GetServerSideProps = (ctx) => {
   return getLocaleProps(ctx);
