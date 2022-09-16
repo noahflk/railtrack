628a0a69e9bb26f2efd16e231d433628ca63248a
Noah <hi@noahflk.com>
2022-09-17 01:16:42 +0200

    Add basic dashboard test

---
+++ tests/dashboard.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@playwright/test';
+
+test.describe('Check dashboard page', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/dashboard');
+  });
+
+  test('get redirected to signin page because not logged in', async ({ page }) => {
+    await expect(page).toHaveTitle(/SignIn/);
+    await expect(page).toHaveURL(/\/signin/);
+  });
+});
