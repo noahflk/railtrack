a59f379cca97274de5e04c8cdfeff22f98650604
Noah <hi@noahflk.com>
2022-09-20 00:35:43 +0200

    Testing something

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -3,12 +3,18 @@ import type { GetServerSideProps, NextPage } from 'next';
 import { AuthWrapper } from '@/components/auth/AuthWrapper';
 import { SignInForm } from '@/components/auth/SignInForm';
 import { protectedAuthWithLocales } from '@/utils/protectedLocales';
+import { supabaseClient } from '@supabase/auth-helpers-nextjs';
 
-const SignIn: NextPage = () => (
-  <AuthWrapper type="signin">
-    <SignInForm />
-  </AuthWrapper>
-);
+const SignIn: NextPage = () => {
+  const user = supabaseClient.auth.user();
+  console.log(user, 'balbla');
+
+  return (
+    <AuthWrapper type="signin">
+      <SignInForm />
+    </AuthWrapper>
+  );
+};
 
 export const getServerSideProps: GetServerSideProps = (ctx) => {
   return protectedAuthWithLocales(ctx);
