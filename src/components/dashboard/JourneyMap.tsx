90d667ab621dc2eb618f543f08043e6364948ebf
Noah <hi@noahflk.com>
2022-10-31 16:48:21 +0200

    Show map with full width during loading

--- src/components/dashboard/JourneyMap.tsx
+++ src/components/dashboard/JourneyMap.tsx
@@ -4,12 +4,9 @@ import { trpc } from '@/utils/trpc';
 export const JourneyMap: React.FC = () => {
   const { data: stats } = trpc.useQuery(['journey.stats']);
 
-  // render map with no journeys while loading them
-  if (!stats) return <Map journeys={[]} />;
-
   return (
     <div className="col-span-1 bg-white divide-y divide-gray-200 rounded-lg shadow lg:col-span-2">
-      <Map journeys={stats.coordinates} />
+      <Map journeys={stats?.coordinates ?? []} />
     </div>
   );
 };
