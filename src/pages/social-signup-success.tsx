98fe5dc3b65a95d65ba5bca1363232fd950d1827
Noah <hi@noahflk.com>
2022-11-02 18:31:50 +0200

    Log google logins

--- src/pages/social-signup-success.tsx
+++ src/pages/social-signup-success.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from 'next';
-import { useUser } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
+
+import { trpc } from '@/utils/trpc';
 
 const SocialSignupSuccess: NextPage = () => {
-  const user = useUser();
+  const router = useRouter();
+  const mutation = trpc.useMutation('log.potential-google-signup');
 
-  console.log(user, 'useeeer');
+  // we do this on client because we don't yet have the access token on server
+  // send request to server to notify for new google login
+  mutation.mutate();
 
-  return <p>Nice to meet you!</p>;
+  // immediately redirect to dashboard
+  router.push('/dashboard');
+
+  return null;
 };
 
 export default SocialSignupSuccess;
