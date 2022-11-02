818112b1685aab04765aad73b5e2406788275706
Noah <hi@noahflk.com>
2022-11-02 23:40:31 +0200

    Only perform the mutation once

--- src/pages/social-signup-success.tsx
+++ src/pages/social-signup-success.tsx
@@ -3,20 +3,22 @@ import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
 import { trpc } from '@/utils/trpc';
+import { useEffect } from 'react';
 
 const SocialSignupSuccess: NextPage = () => {
   const router = useRouter();
   const user = useUser();
   const mutation = trpc.useMutation('log.potential-google-signup');
 
-  // waiting until we have the user locally
-  if (user) {
-    // we do this on client because we don't yet have the access token on server
-    // send request to server to notify for new google login
-    mutation.mutate();
+  useEffect(() => {
+    if (user) {
+      // we do this on client because we don't yet have the access token on server
+      // send request to server to notify for new google login
+      mutation.mutate();
 
-    router.push('/dashboard');
-  }
+      router.push('/dashboard');
+    }
+  }, [user, router, mutation]);
 
   return null;
 };
