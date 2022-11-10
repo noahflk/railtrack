2adb30a8260c97a52983fe824d78da5fd4279060
Noah <hi@noahflk.com>
2022-11-10 17:00:13 +0200

    Don't send logsnag notifications in development env

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -12,7 +12,15 @@ type LogsnagConfig = {
 const API_URL = 'https://api.logsnag.com/v1/log';
 const PROJECT_NAME = 'railtrack';
 
-export const log = async (config: LogsnagConfig) =>
+export const log = async (config: LogsnagConfig) => {
+  const env = process.env.NODE_ENV;
+
+  // don't send logsnag notification in dev environment
+  if (env === 'development') {
+    console.log(config.event, config.description);
+    return;
+  }
+
   axios.post(
     API_URL,
     {
@@ -26,3 +34,4 @@ export const log = async (config: LogsnagConfig) =>
       },
     }
   );
+};
