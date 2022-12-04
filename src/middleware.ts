d687db3db1038af64ca4f33bda40b6b4042f168b
Noah <hi@noahflk.com>
2022-12-04 22:27:46 +0100

    Update auth checker middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,29 @@
-import { withMiddlewareAuth } from '@supabase/auth-helpers-nextjs';
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export const middleware = withMiddlewareAuth({
-  redirectTo: '/signin',
-});
+export async function middleware(req: NextRequest) {
+  // We need to create a response and hand it to the supabase client to be able to modify the response headers.
+  const res = NextResponse.next();
+
+  // Create authenticated Supabase Client.
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // Forward to protected route if we have a session
+  if (session) {
+    return res;
+  }
+
+  // Auth condition not met, redirect to login page.
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = '/signin';
+  redirectUrl.searchParams.set(`redirectedFrom`, req.nextUrl.pathname);
+  return NextResponse.redirect(redirectUrl);
+}
 
 export const config = {
   matcher: ['/dashboard', '/add', '/journeys', '/settings'],
