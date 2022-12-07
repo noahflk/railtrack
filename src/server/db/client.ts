6ab786386179aed0528a3c48f653c590ac83997f
Noah <hi@noahflk.com>
2022-12-07 20:03:13 +0100

    Stop Prisma from logging queries

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -8,7 +8,7 @@ declare global {
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: ['info', 'warn', 'error'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
