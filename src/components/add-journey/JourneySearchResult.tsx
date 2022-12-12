65c818f98cd8805f60ec59fe9c52481f8618ab71
Noah <hi@noahflk.com>
2022-12-12 19:46:05 +0100

    Ignore empty product entries for search results

--- src/components/add-journey/JourneySearchResult.tsx
+++ src/components/add-journey/JourneySearchResult.tsx
@@ -64,14 +64,17 @@ const JourneyHeader: React.FC<Props> = ({ journey }) => {
   const t = useTranslations('add');
 
   const firstValidSection = journey.sections.find((section) => section.journey);
+  const firstNonEmptyProduct = journey.products.find((product) => product !== ' ');
 
   if (!firstValidSection) return null;
 
   return (
     <p>
-      <span className="mr-2 inline-flex items-center rounded-full bg-primary px-2.5 py-1 text-sm font-medium text-white">
-        {journey.products[0]}
-      </span>
+      {firstNonEmptyProduct && (
+        <span className="mr-2 inline-flex items-center rounded-full bg-primary px-2.5 py-1 text-sm font-medium text-white">
+          {firstNonEmptyProduct}
+        </span>
+      )}
       {t('direction')} {firstValidSection.journey?.to}
     </p>
   );
