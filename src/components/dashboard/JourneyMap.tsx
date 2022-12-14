3161f60c767b91786bd069354d4fc05f3c96402d
Noah <hi@noahflk.com>
2022-12-14 16:32:45 +0100

    Center map when changing pages

--- src/components/dashboard/JourneyMap.tsx
+++ src/components/dashboard/JourneyMap.tsx
@@ -1,12 +1,7 @@
 import { Map } from '@/components/Map';
-import { trpc } from '@/utils/trpc';
 
-export const JourneyMap: React.FC = () => {
-  const { data: stats } = trpc.journey.stats.useQuery();
-
-  return (
-    <div className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow lg:col-span-2">
-      <Map journeys={stats?.coordinates ?? []} />
-    </div>
-  );
-};
+export const JourneyMap: React.FC = () => (
+  <div className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow lg:col-span-2">
+    <Map />
+  </div>
+);
