4ed5049a2ca6e9f1d8ee8032f1ecaf3d5d899732
Mohammed <hi@cherfaoui.dev>
2023-01-31 21:52:15 +0100

    small improvements, clear unnecessary logic

--- src/components/add-journey/JourneySectionsDetails.tsx
+++ src/components/add-journey/JourneySectionsDetails.tsx
@@ -6,7 +6,6 @@ import { type Pass, type Journey } from '@/types/opendata';
 import { formatDateTime } from '@/utils/formatDateTime';
 import { classNames } from '@/utils/styling';
 import { type Dispatch, Fragment, type SetStateAction, useState } from 'react';
-import md5 from 'md5';
 
 type Props = {
   journey: Journey;
@@ -30,39 +29,33 @@ export const JourneySectionsDetails: React.FC<Props> = ({ journey }) => {
         <div className="px-4 md:px-5">
           <hr className="mb-6 h-px w-full border-0 bg-gray-200" />
         </div>
-        <div className="flex flex-col px-8 md:px-10 pb-6">
-          {nonNullJourneys.map((section, index) => {
-            const journeyId = md5(
-              `${section.journey?.passList[0]?.departure}${section.journey?.passList[0]?.arrival}${section.journey?.passList[0]?.location.id}`
-            );
-
-            return (
-              <Fragment key={index}>
-                <div className="mb-8">
-                  <p>
-                    <span className="mr-2 inline-flex items-center rounded-full bg-primary px-2.5 py-1 text-sm font-medium text-white">
-                      {journey.products[index]}
-                    </span>
-                    {t('direction')} {section.journey?.to}
-                  </p>
-                </div>
-                <div
-                  key={index}
-                  className="flex min-h-[50px] flex-col  [&_div:first-child_.bullet]:mt-2 [&_div:last-child_.bullet]:border-l-0"
-                >
-                  <JourneySectionsPreview journeyId={journeyId} passList={section.journey?.passList} />
-                </div>
-                <>
-                  {index !== nonNullJourneys.length - 1 && (
-                    <div className="my-8 flex space-x-8 rounded bg-gray-100 py-3 px-6">
-                      <RefreshIcon className="h-6 w-6 text-gray-600" />
-                      <p className="text-sm text-gray-600">{t('trainChange')}</p>
-                    </div>
-                  )}
-                </>
-              </Fragment>
-            );
-          })}
+        <div className="flex flex-col px-8 pb-6 md:px-10">
+          {nonNullJourneys.map((section, index) => (
+            <Fragment key={index}>
+              <div className="mb-8">
+                <p>
+                  <span className="mr-2 inline-flex items-center rounded-full bg-primary px-2.5 py-1 text-sm font-medium text-white">
+                    {journey.products[index]}
+                  </span>
+                  {t('direction')} {section.journey?.to}
+                </p>
+              </div>
+              <div
+                key={index}
+                className="flex min-h-[50px] flex-col  [&_div:first-child_.bullet]:mt-2 [&_div:last-child_.bullet]:border-l-0"
+              >
+                <JourneySectionsPreview passList={section.journey?.passList} />
+              </div>
+              <>
+                {index !== nonNullJourneys.length - 1 && (
+                  <div className="my-8 flex space-x-8 rounded bg-gray-100 py-3 px-6">
+                    <RefreshIcon className="h-6 w-6 text-gray-600" />
+                    <p className="text-sm text-gray-600">{t('trainChange')}</p>
+                  </div>
+                )}
+              </>
+            </Fragment>
+          ))}
         </div>
       </Disclosure.Panel>
     </Transition>
@@ -70,12 +63,11 @@ export const JourneySectionsDetails: React.FC<Props> = ({ journey }) => {
 };
 
 type JourneySectionsPreviewProps = {
-  journeyId: string;
   passList: Pass[] | undefined;
 };
 
-const JourneySectionsPreview = ({ passList, journeyId }: JourneySectionsPreviewProps) => {
-  const [expandPasslist, setExpandPasslist] = useState('');
+const JourneySectionsPreview: React.FC<JourneySectionsPreviewProps> = ({ passList }) => {
+  const [expandPasslist, setExpandPasslist] = useState(false);
   const lastItemIndex = passList && passList.length - 1;
 
   return (
@@ -87,7 +79,6 @@ const JourneySectionsPreview = ({ passList, journeyId }: JourneySectionsPreviewP
               <JourneyPasslist
                 key={index}
                 pass={pass}
-                journeyId={journeyId}
                 extraProps={{ index, firstOrLastItem, lastItemIndex, passListLength: passList.length }}
                 expandPasslist={{ state: expandPasslist, setState: setExpandPasslist }}
               />
@@ -99,7 +90,6 @@ const JourneySectionsPreview = ({ passList, journeyId }: JourneySectionsPreviewP
               <JourneyPasslist
                 key={index}
                 pass={pass}
-                journeyId={journeyId}
                 extraProps={{ index, firstOrLastItem, lastItemIndex: 1, passListLength: passList?.length }}
                 expandPasslist={{ state: expandPasslist, setState: setExpandPasslist }}
               />
@@ -111,7 +101,6 @@ const JourneySectionsPreview = ({ passList, journeyId }: JourneySectionsPreviewP
 
 type JourneyPasslistProps = {
   pass: Pass | undefined;
-  journeyId: string;
   extraProps: {
     index: number;
     lastItemIndex: number | undefined;
@@ -119,11 +108,12 @@ type JourneyPasslistProps = {
     passListLength: number | undefined;
   };
   expandPasslist: {
-    state: string;
-    setState: Dispatch<SetStateAction<string>>;
+    state: boolean;
+    setState: Dispatch<SetStateAction<boolean>>;
   };
 };
-const JourneyPasslist: React.FC<JourneyPasslistProps> = ({ pass, extraProps, expandPasslist, journeyId }) => {
+
+const JourneyPasslist: React.FC<JourneyPasslistProps> = ({ pass, extraProps, expandPasslist }) => {
   return (
     <div className="flex">
       <ul className="flex min-w-[40px] flex-[0_0_3.5em] [&_li]:mb-1 [&_p]:leading-none">
@@ -158,15 +148,9 @@ const JourneyPasslist: React.FC<JourneyPasslistProps> = ({ pass, extraProps, exp
         {extraProps.passListLength! > 2 && extraProps.index === 0 && (
           <button
             className="absolute top-[47%] -ml-[.7em] rounded-[50%] border-[1px] border-black bg-white  hover:border-primary"
-            onClick={() => {
-              if (expandPasslist.state) {
-                expandPasslist.setState('');
-              } else {
-                expandPasslist.setState(journeyId);
-              }
-            }}
+            onClick={() => expandPasslist.setState((state) => !state)}
           >
-            {expandPasslist.state === journeyId ? (
+            {expandPasslist.state ? (
               <MinusSmIcon className="h-5 w-5 text-black hover:text-primary" />
             ) : (
               <PlusSmIcon className="h-5 w-5 text-black hover:text-primary" />
